93d6e44e34b2cc039db91679de5ea8daae1e24bc
Danielk10 <danielpdiamon@gmail.com>
2024-06-07 11:00:38 -0400

    update

--- naaraWeb/naaraWeb/urls.py
+++ naaraWeb/naaraWeb/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path ,include, re_path
+from django.urls import path ,include
 from django.conf import settings
-from django.views.static import serve
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -26,15 +27,6 @@ urlpatterns = [
  
 ]
 
-
-#codigo para cargar imágenes
-
-  
-urlpatterns +=[
-    re_path(r"^media/(?P<path>.*)$",serve,{
-      "document_root":settings.MEDIA_ROOT,
-    }),
-  ]
-  
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
