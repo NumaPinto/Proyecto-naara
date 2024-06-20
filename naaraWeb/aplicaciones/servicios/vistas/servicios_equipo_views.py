33e6a4c4d0e0ab30d41bb947a3757bb4852a7765
Danielk10 <danielpdiamon@gmail.com>
2024-06-20 17:32:03 -0400

    update servicios_equipo_views.py

--- naaraWeb/aplicaciones/servicios/vistas/servicios_equipo_views.py
+++ naaraWeb/aplicaciones/servicios/vistas/servicios_equipo_views.py
@@ -43,41 +43,42 @@ class TeamView(TemplateView):
      for a in range(len(datos)):
        
        if datos[a].especialidad_especialista.startswith("Enfermera"):
+         
        
-       especialistas.append(datos[a])
+         especialistas.append(datos[a])
        
      for b in range(len(datos)):
        
        if datos[b].especialidad_especialista.startswith("Cosmetóloga"):
        
-       especialistas.append(datos[b])
+         especialistas.append(datos[b])
 
        
      for c in range(len(datos)):
        
        if datos[c].especialidad_especialista.startswith("Masoterapeuta"):
        
-       especialistas.append(datos[c])
+         especialistas.append(datos[c])
 
        
      for d in range(len(datos)):
        
        if datos[d].especialidad_especialista.startswith("Estilista"):
          
-       especialistas.append(datos[d])
+         especialistas.append(datos[d])
 
        
      for f in range(len(datos)):
        
        if datos[f].especialidad_especialista.startswith("Manicurista"):
        
-       especialistas.append(datos[f])
+         especialistas.append(datos[f])
        
      for g in range(len(datos)):
        
        if datos[g].especialidad_especialista.startswith("Atención al Cliente"):
        
-       especialistas.append(datos[g])
+         especialistas.append(datos[g])
        
      return especialistas
      
