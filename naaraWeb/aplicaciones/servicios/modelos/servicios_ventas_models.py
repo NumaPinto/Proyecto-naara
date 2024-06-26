2c06b5ee1973ca273d8f7f74aa414bb868a1aec5
Danielk10 <danielpdiamon@gmail.com>
2024-06-26 07:52:08 -0400

    update

--- naaraWeb/aplicaciones/servicios/modelos/servicios_ventas_models.py
+++ naaraWeb/aplicaciones/servicios/modelos/servicios_ventas_models.py
@@ -6,7 +6,7 @@ class Servicio(ModeloBase):
     nombre_servicio = models.CharField("Nombre del Servicio",max_length=100)
     descripcion_servicio = models.TextField("Descripción del Servicio")
     precio_servicio = models.DecimalField("Precio del Servicio",max_digits=10, decimal_places=2)
-    imagen_servicio = models.ImageField("Imagen del Servicio",upload_to="servicios/imagenes/servicios/", blank=False, null=False)
+    imagen_servicio = models.ImageField("Imagen del Servicio",upload_to="servicios/imagenes/servicios/", blank=True, null=True)
     
     class Meta:
       verbose_name="Servicio"
@@ -21,7 +21,7 @@ class Producto(ModeloBase):
     descripcion_producto = models.TextField("Descripción del Producto")
     precio_producto = models.DecimalField("Precio del Producto",max_digits=10, decimal_places=2)
     cantidad_disponible_producto = models.PositiveIntegerField("Cantidad de Productos")
-    imagen_producto = models.ImageField("Imagen del Producto",upload_to="servicios/imagenes/productos/", blank=False, null=False)
+    imagen_producto = models.ImageField("Imagen del Producto",upload_to="servicios/imagenes/productos/", blank=True, null=True)
     
     class Meta:
       verbose_name="Producto"
