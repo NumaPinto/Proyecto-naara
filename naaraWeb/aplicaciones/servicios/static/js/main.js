1700538783fa7ab835ebd11201e30905fb1a6034
Danielk10 <danielpdiamon@gmail.com>
2024-06-26 09:26:46 -0400

    update

--- naaraWeb/aplicaciones/servicios/static/js/main.js
+++ naaraWeb/aplicaciones/servicios/static/js/main.js
@@ -107,3 +107,13 @@
     
 })(jQuery);
 
+
+$(function() {
+  $('#datetimepicker1').datetimepicker({
+    format: 'L'
+  });
+  $('#datetimepicker2').datetimepicker({
+    format: 'LT'
+  });
+});
+
